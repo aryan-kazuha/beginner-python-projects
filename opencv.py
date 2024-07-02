d4447d0f9ced4b6fb8fb3e1d71b281209ded2ca2
aryan-kazuha <166980982+aryan-kazuha@users.noreply.github.com>
2024-07-02 23:27:58 +0530

    Add files via upload

---
+++ opencv.py
@@ -0,0 +1,21 @@
+import cv2
+
+# variables
+height = 320
+width = 180
+FPS = 144
+cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
+cam.set(cv2.CAP_PROP_FPS,FPS)
+cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*"MJPG"))
+
+while True:
+    ignore,frame = cam.read()
+    cv2.imshow("my cam",frame)
+    cv2.moveWindow("my cam",0,0)
+
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        break
+cam.release()
